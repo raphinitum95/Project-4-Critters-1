279d32b826322a0d99bcee160c819994ce0b9e5b
Raphael De Los Santos <raphaeldlst@sekg.net>
2016-10-16 13:52:17 -0500

    Start of project

---
+++ Project 4 Critters 1/src/assignment4/Header.java
@@ -0,0 +1,13 @@
+package assignment4;
+/* CRITTERS <MyClass.java>
+ * EE422C Project 4 submission by
+ * Replace <...> with your actual data.
+ * Raphael De Los Santos
+ * rd23353
+ * <Student1 5-digit Unique No.>
+ * JohnnyAngel Rojas
+ * <Student2 EID>
+ * <Student2 5-digit Unique No.>
+ * Slip days used: <0>
+ * Fall 2016
+ */
